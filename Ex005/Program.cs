9c86b95f16a99c049c414619d91eda53c007b279
Roman Kozhin <kozhin.rm@gmail.com>
2022-12-12 03:34:11 +0300

    Вывести приветствие любимому пользователю

---
+++ Ex005/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вывести приветствие любимому пользователю
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
